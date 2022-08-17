affb95f16f12b14d3e2248cdcc3d45f67a530ab0
ws <iam@wsh.dev>
2022-08-17 19:15:04 +0300

    Add svg icons

---
+++ src/ui/Icons/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as LoaderIcon } from "../assets/loader.svg"
+export { ReactComponent as ErrorIcon } from "../assets/error.svg"
+export { ReactComponent as SearchIcon } from "../assets/search.svg"
