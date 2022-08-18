4b08775092822aea1220f0604065cb3ed6b94ed5
ws <iam@wsh.dev>
2022-08-18 11:52:10 +0300

    Improve UsersTable helpers

--- src/components/UsersTable/helpers.tsx
+++ src/components/UsersTable/helpers.tsx
@@ -1,12 +1,7 @@
 import { Fragment } from "react"
-import type { ReactNode } from "react"
 import cn from "classnames"
 
 import { UserType } from "../../api"
-
-type ElProps = {
-  children?: ReactNode | ReactNode[] | null
-}
 
 function getHighlightedText(text: string, highlight?: string) {
   if (!highlight) {
@@ -25,57 +20,48 @@ function getHighlightedText(text: string, highlight?: string) {
   ))
 }
 
-function TH({ children }: ElProps) {
-  return (
-    <th
-      className={cn(
-        "border",
-        "border-slate-300",
-        "dark:border-slate-600",
-        "font-semibold",
-        "p-4",
-        "text-slate-900",
-        "dark:text-slate-200",
-        "text-left",
-        "bg-slate-50",
-        "dark:bg-slate-700"
-      )}
-    >
-      {children}
-    </th>
+export const renderRow = (user: UserType, highlight?: string) => {
+  const classNames = cn(
+    "border-b",
+    "border-slate-300",
+    "dark:border-slate-700",
+    "p-4",
+    "text-slate-500",
+    "dark:text-slate-400"
   )
-}
-function TD({ children }: ElProps) {
+
   return (
-    <td
-      className={cn(
-        "border",
-        "border-slate-300",
-        "dark:border-slate-700",
-        "p-4",
-        "text-slate-500",
-        "dark:text-slate-400"
-      )}
-    >
-      {children}
-    </td>
+    <>
+      <td className={classNames}>
+        {getHighlightedText(user.firstName, highlight)}
+      </td>
+      <td className={classNames}>
+        {getHighlightedText(user.surname, highlight)}
+      </td>
+      <td className={classNames}>{user.phone}</td>
+      <td className={classNames}>{user.phone}</td>
+    </>
   )
 }
 
-export const renderRow = (user: UserType, highlight?: string) => (
-  <>
-    <TD>{getHighlightedText(user.firstName, highlight)}</TD>
-    <TD>{getHighlightedText(user.surname, highlight)}</TD>
-    <TD>{user.phone}</TD>
-    <TD>{user.phone}</TD>
-  </>
-)
+export const renderHeader = () => {
+  const classNames = cn(
+    "font-semibold",
+    "p-4",
+    "text-slate-900",
+    "dark:text-slate-200",
+    "text-left",
+    "bg-slate-50",
+    "dark:bg-slate-700",
+    "w-1/4"
+  )
 
-export const renderHeader = () => (
-  <tr>
-    <TH>First name</TH>
-    <TH>Surname</TH>
-    <TH>Phone</TH>
-    <TH>Email</TH>
-  </tr>
-)
+  return (
+    <tr>
+      <th className={classNames}>First name</th>
+      <th className={classNames}>Surname</th>
+      <th className={classNames}>Phone</th>
+      <th className={classNames}>Email</th>
+    </tr>
+  )
+}
